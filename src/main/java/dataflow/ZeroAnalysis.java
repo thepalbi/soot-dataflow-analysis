8ec25f2299357512bae24681bcff73773826b3b6
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-09-10 21:58:13 -0300

    WIP

--- src/main/java/dataflow/ZeroAnalysis.java
+++ src/main/java/dataflow/ZeroAnalysis.java
@@ -1,31 +1,57 @@
 package dataflow;
 
-import dataflow.abs.ZeroLattice;
-import soot.Unit;
+import soot.IntType;
+import soot.Value;
+import soot.jimple.BinopExpr;
+import soot.jimple.DefinitionStmt;
+import soot.jimple.Stmt;
 import soot.toolkits.graph.DirectedGraph;
+import soot.toolkits.scalar.FlowSet;
 import soot.toolkits.scalar.ForwardFlowAnalysis;
 
-public class ZeroAnalysis extends ForwardFlowAnalysis<Unit, ZeroLattice> {
+/**
+ * Division by zero analysis
+ */
+public class ZeroAnalysis extends ForwardFlowAnalysis<Stmt, FlowSet> {
 
-  public ZeroAnalysis(DirectedGraph<Unit> graph) {
+  public ZeroAnalysis(DirectedGraph<Stmt> graph) {
     super(graph);
-    // Internal structures initialization here
     doAnalysis();
   }
 
-  protected void flowThrough(ZeroLattice zeroLattice, Unit unit, ZeroLattice a1) {
+  protected void flowThrough(FlowSet flowSet, Stmt unit, FlowSet a1) {
 
+    // It's an assignment / definition
+    if (unit instanceof DefinitionStmt) {
+      DefinitionStmt newDefinition = (DefinitionStmt) unit;
+      Value variableName = newDefinition.getLeftOp();
+      Value value = newDefinition.getRightOp();
+
+      // The assigned value is int, check if an the right value is an operation or a simple assignment
+      if (value.getType() instanceof IntType) {
+        if (value instanceof BinopExpr) {
+          BinopExpr operation = (BinopExpr) value;
+          if (operation.getSymbol().equalsIgnoreCase(" / ")) {
+            // Handle division by ZERO
+
+          } else {
+
+          }
+        }
+
+      }
+    }
   }
 
-  protected ZeroLattice newInitialFlow() {
+  protected FlowSet newInitialFlow() {
     return null;
   }
 
-  protected void merge(ZeroLattice zeroLattice, ZeroLattice a1, ZeroLattice a2) {
+  protected void merge(FlowSet flowSet, FlowSet a1, FlowSet a2) {
 
   }
 
-  protected void copy(ZeroLattice zeroLattice, ZeroLattice a1) {
+  protected void copy(FlowSet flowSet, FlowSet a1) {
 
   }
 }
