45c29fb05aed9eaf65d6e4e68fba0056183d2d46
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-09-23 00:50:21 -0300

    Typo in TODO

--- src/main/java/dataflow/DivisionByZeroAnalysis.java
+++ src/main/java/dataflow/DivisionByZeroAnalysis.java
@@ -57,7 +57,7 @@ public class DivisionByZeroAnalysis extends ForwardFlowAnalysis<Unit, VariableTo
   }
 
   protected void merge(VariableToLatticeMap input1, VariableToLatticeMap input2, VariableToLatticeMap output) {
-    // TODO: Have to the the supreme of both values if there are conflicting keys
+    // TODO: Have to add the supreme of both values if there are conflicting keys
     output.putAll(input1);
     output.putAll(input2);
   }
