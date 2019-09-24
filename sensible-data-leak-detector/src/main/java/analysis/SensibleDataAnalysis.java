2f3afc972300949ad942ff39b8d8f91b91480b15
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-09-24 00:07:12 -0300

    Initial values, merge and copy

--- sensible-data-leak-detector/src/main/java/analysis/SensibleDataAnalysis.java
+++ sensible-data-leak-detector/src/main/java/analysis/SensibleDataAnalysis.java
@@ -1,16 +1,32 @@
 package analysis;
 
+import static analysis.abstraction.SensibilityLattice.BOTTOM;
+import static analysis.abstraction.SensibilityLattice.supremeBetween;
+
+import java.util.HashMap;
 import java.util.Map;
 
 import analysis.abstraction.SensibilityLattice;
+import soot.Local;
 import soot.Unit;
 import soot.toolkits.graph.ExceptionalUnitGraph;
 import soot.toolkits.scalar.ForwardFlowAnalysis;
 
 public class SensibleDataAnalysis extends ForwardFlowAnalysis<Unit, Map<String, SensibilityLattice>> {
 
+  private Map<String, SensibilityLattice> startingLocalsMap;
+  private Map<Unit, Boolean> possibleLeakInUnit;
+
   public SensibleDataAnalysis(ExceptionalUnitGraph graph) {
     super(graph);
+
+    startingLocalsMap = new HashMap<>();
+    possibleLeakInUnit = new HashMap<>();
+
+    // As starting point, save all locals as bottom
+    for (Local variable : graph.getBody().getLocals()) {
+      startingLocalsMap.put(variable.getName(), SensibilityLattice.getBottom());
+    }
 
     doAnalysis();
   }
@@ -23,12 +39,21 @@ public class SensibleDataAnalysis extends ForwardFlowAnalysis<Unit, Map<String, 
 
   @Override
   protected Map<String, SensibilityLattice> newInitialFlow() {
-    return null;
+    Map<String, SensibilityLattice> newMap = new HashMap<>();
+    newMap.putAll(startingLocalsMap);
+    return newMap;
   }
 
   @Override
   protected void merge(Map<String, SensibilityLattice> input1, Map<String, SensibilityLattice> input2,
                        Map<String, SensibilityLattice> out) {
+    out.clear();
+    out.putAll(input1);
+    // On conflicting values, take supreme to make analysis sound
+    for (String variable : input2.keySet()) {
+      SensibilityLattice currentValue = out.getOrDefault(variable, BOTTOM);
+      out.put(variable, supremeBetween(input2.get(variable), currentValue));
+    }
 
   }
 
