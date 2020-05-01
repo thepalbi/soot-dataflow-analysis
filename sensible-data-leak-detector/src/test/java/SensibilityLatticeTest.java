286989645effe8409ea5173bdcf53d57c9546cb2
Pablo Balbi <pablo.l.balbi@gmail.com>
2020-05-01 17:14:23 -0300

    Junit 5. Fix lattice tests

--- sensible-data-leak-detector/src/test/java/SensibilityLatticeTest.java
+++ sensible-data-leak-detector/src/test/java/SensibilityLatticeTest.java
@@ -1,43 +1,27 @@
 import analysis.abstraction.SensibilityLattice;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Stream;
 
 import static analysis.abstraction.SensibilityLattice.*;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 
-@RunWith(Parameterized.class)
 public class SensibilityLatticeTest {
 
-    private SensibilityLattice operator1;
-    private SensibilityLattice operator2;
-    private SensibilityLattice expected;
-
-
-    @Parameterized.Parameters(name = "{3}")
-    public static List<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {NOT_SENSIBLE, HIGH, MAYBE_SENSIBLE, "top is returned on same level sensibilites"},
-                {NOT_SENSIBLE, NOT_SENSIBLE, NOT_SENSIBLE, "supreme of equal values"},
-                {NOT_SENSIBLE, MAYBE_SENSIBLE, MAYBE_SENSIBLE, "top acts as sink"},
-                {NOT_SENSIBLE, BOTTOM, NOT_SENSIBLE, "bottom acts as neuter"},
-        });
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of(NOT_SENSIBLE, HIGH, MAYBE_SENSIBLE),
+                Arguments.of(NOT_SENSIBLE, NOT_SENSIBLE, NOT_SENSIBLE),
+                Arguments.of(NOT_SENSIBLE, MAYBE_SENSIBLE, MAYBE_SENSIBLE),
+                Arguments.of(NOT_SENSIBLE, BOTTOM, NOT_SENSIBLE)
+        );
     }
 
-    public SensibilityLatticeTest(SensibilityLattice operator1, SensibilityLattice operator2, SensibilityLattice expected,
-                                  String name) {
-        this.operator1 = operator1;
-        this.operator2 = operator2;
-        this.expected = expected;
-    }
-
-    @Test
-    public void testSupreme() {
-        assertThat(supremeBetween(operator1, operator2),
-                is(expected));
+    @ParameterizedTest
+    @MethodSource("data")
+    public void testSupreme(SensibilityLattice operator1, SensibilityLattice operator2, SensibilityLattice expected) {
+        Assertions.assertEquals(expected, supremeBetween(operator1, operator2));
     }
 }
